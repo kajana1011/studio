8ed5a2e3eec6fbf5f7e873dbeec82f211e8b1cc7
revocajana <revocajana@gmail.com>
2025-09-04 10:53:52 +0300

    online scrpt instead of offline /js/bundle.min.js

--- includes/footer.php
+++ includes/footer.php
@@ -84,8 +84,8 @@
         <i class="fab fa-whatsapp"></i>
     </a>
 
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
-     <script src="assets/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+     <!-- <script src="assets/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script> -->
     
     <!-- Custom JS -->
     <script src="assets/js/scrpt.js"></script>
